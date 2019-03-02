b5ac878f8accf601d8ae59e8e933a6ce49c3ce12
bartmilew358 <bartmilew@gmail.com>
2019-03-02 10:21:55 +0100

    add coments // just test commit

--- src/FamilyTest.java
+++ src/FamilyTest.java
@@ -1,11 +1,35 @@
+//public class FamilyTest {
+//
+//    public static void main(String[] args) {
+//
+//        FamilyTree family1 = new FamilyTree("Czarkowscy", "Cezary", "Czarkowski", 22, "Cecylia", "Czarkowska", 24, "Czygomir", "Czarkowski", 2);
+//        FamilyTree family2 = new FamilyTree("Greenstein", "Szmailo", "Greenstein", 30, "Tamara", "Greenstein", 28, "Adam", "Greenstein", 6);
+//
+//        System.out.println(family1.family());
+//        System.out.println(family2.family());
+//    }
+//}
+
+
 public class FamilyTest {
+
+//    Person ojciec = new Person("Janusz", "Elton", 28);
 
     public static void main(String[] args) {
 
-        FamilyTree family1 = new FamilyTree("Czarkowscy", "Cezary", "Czarkowski", 22, "Cecylia", "Czarkowska", 24, "Czygomir", "Czarkowski", 2);
-        FamilyTree family2 = new FamilyTree("Greenstein", "Szmailo", "Greenstein", 30, "Tamara", "Greenstein", 28, "Adam", "Greenstein", 6);
+        Person husband1 = new Person("Ryszard", "Ochucki", 44);
+        System.out.println("husband 1 = " + husband1);
 
-        System.out.println(family1.family());
-        System.out.println(family2.family());
+        Person wife1 = new Person("a", "b", 20);
+        Person child1 = new Person("d", "e", 30);
+
+        Family family1 = new Family(husband1, wife1, child1, "Okoń");
+        System.out.println("family1 = " + family1);
+
+
     }
+
+
 }
+
+
---
+++ src/Person.java
@@ -0,0 +1,34 @@
+public class Person {
+
+    private String personName;
+    private String personSurname;
+    private int personAge;
+
+
+    public Person(String personName, String personSurname, int personAge) {
+        this.personName = personName;
+        this.personSurname = personSurname;
+        this.personAge = personAge;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public String getPersonSurname() {
+        return personSurname;
+    }
+
+    public int getPersonAge() {
+        return personAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "personName='" + personName + '\'' +
+                ", personSurname='" + personSurname + '\'' +
+                ", personAge=" + personAge +
+                '}';
+    }
+}
